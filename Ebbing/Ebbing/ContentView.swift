8c88d2b72c09208eec23aa05c80ad6208d1fd49d
Junmung <9174844@naver.com>
2020-08-17 18:48:36 +0900

    first commit

---
+++ Ebbing/Ebbing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ebbing
+//
+//  Created by 준멍 on 2020/08/17.
+//  Copyright © 2020 Junsung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
